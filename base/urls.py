bb09d30483f53ec04dd7e5f1a64434ecce1665c9
dan0307 <daniyalbayanov@gmail.com>
2022-11-02 13:50:36 +0600

    changes reg

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path('', views.home, name="home"),
-    path('admin/', admin.site.urls),
-    path('register/', views.registerPage, name="register"),
-    path('login/', views.loginPage, name="login"),
+    path('home_page', views.home, name="home"),
+    path('register/', views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
+from .forms import CreateUserForm, RegisterForm
+from django.contrib import messages
 # Create your views here.
 # This is our links?
 
@@ -11,14 +12,33 @@ def home(request):
 def aboutus(request):
     return HttpResponse('About us')
 
+def register(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        context = {'form' : form}
+        return render(request, 'register.html', context)
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+        else:
+            print('Form is not valid')
+            messages.error(request, 'Error Processing Your Request')
+            context = {'form': form}
+            return render(request, 'register.html', context)
+
+    return render(request, 'register.html', {})
 def registerPage(request):
-    form = CreateUserForm()
+    form = CreateUserForm(request.POST)
 
     if request.method == 'POST':
          form = UserCreationForm(request.POST)
          if form .is_valid():
              form.save()
-
+    messages.success(request, 'Account created successfully')
     context = {'form':form}
     return render(request,'register.html',context)
 
